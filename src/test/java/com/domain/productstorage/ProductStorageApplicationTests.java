050a4dfb9d39a3910371166416d2ae6fb525384b
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-10-13 20:31:09 -0300

    projeto criado

---
+++ src/test/java/com/domain/productstorage/ProductStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.productstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
