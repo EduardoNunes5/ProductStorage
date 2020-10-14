f882bb3e5180565f43994cb40aaa67f873323de0
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-10-13 21:21:40 -0300

    entidade produto

--- src/test/java/com/domain/productstorage/ProdutoStorageApplicationTests.java
+++ src/test/java/com/domain/productstorage/ProdutoStorageApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductStorageApplicationTests {
+class ProdutoStorageApplicationTests {
 
     @Test
     void contextLoads() {
