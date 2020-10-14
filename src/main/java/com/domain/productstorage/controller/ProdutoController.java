ccf819a96347b80f195ba2b708080bcd09544d9f
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-10-13 22:56:04 -0300

    deleta produto

--- src/main/java/com/domain/productstorage/controller/ProdutoController.java
+++ src/main/java/com/domain/productstorage/controller/ProdutoController.java
@@ -1,7 +1,9 @@
 package com.domain.productstorage.controller;
 
 import com.domain.productstorage.entity.Produto;
+import com.domain.productstorage.exceptions.ProdutoNaoEncontrado;
 import com.domain.productstorage.services.ProdutoServiceI;
+import com.domain.productstorage.util.MensagemErro;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,5 +27,25 @@ public class ProdutoController {
     @GetMapping(value="/produtos")
     public ResponseEntity<?> getProdutos(){
         return new ResponseEntity<>(this.produtoService.findAll(), HttpStatus.OK);
+    }
+
+    @PutMapping(value="/produtos/{id}")
+    public ResponseEntity<?> atualizaProduto(@PathVariable("id") long id, @RequestBody Produto produto){
+        try{
+            return new ResponseEntity<>(this.produtoService.atualizaProduto(id, produto), HttpStatus.OK);
+        }
+        catch(ProdutoNaoEncontrado pne){
+            return new ResponseEntity<>(new MensagemErro(pne.getMessage()), HttpStatus.NOT_FOUND);
+        }
+
+    }
+    @DeleteMapping(value="/produtos/{id}")
+    public ResponseEntity<?> deletaProduto(@PathVariable("id") Long id){
+        try{
+            return new ResponseEntity<>(this.produtoService.deletaProduto(id), HttpStatus.OK);
+        }
+        catch(ProdutoNaoEncontrado pne){
+            return new ResponseEntity<>(new MensagemErro(pne.getMessage()), HttpStatus.NOT_FOUND);
+        }
     }
 }
