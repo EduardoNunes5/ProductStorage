a22e53cdf8f908352f67d63f9146843d0fea5329
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-10-13 22:55:46 -0300

    atualiza produto

--- src/main/java/com/domain/productstorage/services/ProdutoServiceI.java
+++ src/main/java/com/domain/productstorage/services/ProdutoServiceI.java
@@ -1,6 +1,7 @@
 package com.domain.productstorage.services;
 
 import com.domain.productstorage.entity.Produto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,4 +12,8 @@ public interface ProdutoServiceI {
     Produto insert(Produto produto);
 
     List<Produto> findAll();
+
+    Produto deletaProduto(Long id);
+
+    Produto atualizaProduto(long id, Produto produto);
 }
