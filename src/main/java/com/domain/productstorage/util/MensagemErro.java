7531682d414442ef0352ad4e5d3069da9425a9e9
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-10-13 22:55:15 -0300

    util msg de erro

---
+++ src/main/java/com/domain/productstorage/util/MensagemErro.java
@@ -0,0 +1,18 @@
+package com.domain.productstorage.util;
+
+public class MensagemErro {
+
+    String erro;
+
+    public MensagemErro(String erro) {
+        this.erro = erro;
+    }
+
+    public String getErro() {
+        return erro;
+    }
+
+    public void setErro(String erro) {
+        this.erro = erro;
+    }
+}
